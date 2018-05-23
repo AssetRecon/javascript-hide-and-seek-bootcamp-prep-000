cb533a946a59030d568b86c7063a47b6e6f37872
Edsel Caprice <caprice@mail.usf.edu>
2018-05-23 01:24:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector);
+}
+
+function nestedTarget() {
+return document.querySelector('#nested .target');
+}
+
+
+var ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+function increaseRankBy(n) {
+  for (let i = 0; i < ranks.length; i++ ) {
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
+
+
+
+var deepest = document.querySelectorAll('#grand-node div');
+function deepestChild() {
+     return deepest[deepest.length-1];
+}
+
